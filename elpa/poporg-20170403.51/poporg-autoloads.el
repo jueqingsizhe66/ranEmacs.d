db93d69c9ca7bd75b640ee37e8baefc5345581f7
Ye Zhaoliang <zhaoturkkey@163.com>
2018-05-10 14:56:16 +0800

    unlock the world,unlock new knowledge

---
+++ elpa/poporg-20170403.51/poporg-autoloads.el
@@ -0,0 +1,47 @@
+;;; poporg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "poporg" "poporg.el" (23282 27023 0 0))
+;;; Generated autoloads from poporg.el
+
+(autoload 'poporg-dwim "poporg" "\
+Single overall command for poporg (a single keybinding may do it all).
+
+If the current buffer is an edit buffer, run `poporg-edit-exit'.
+
+If the region is active, edit it in an empty buffer.  Otherwise, find a nearby
+string or comment using `poporg-find-string-or-comment' and edit that in an
+empty buffer.  If there is an active edit nearby, pop to its other buffer and
+edit that instead.
+
+\(fn)" t nil)
+
+(autoload 'poporg-update "poporg" "\
+Update the contents of the original buffer.
+
+If prefix argument WITH-SAVE is non-nil, save the original buffer too.
+
+Also update the overlay.
+
+\(fn WITH-SAVE)" t nil)
+
+(autoload 'poporg-update-and-save "poporg" "\
+Update and save the original buffer; update the region.
+
+\(fn)" t nil)
+
+(autoload 'poporg-edit-exit "poporg" "\
+Exit the edit buffer, replacing the original region.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; poporg-autoloads.el ends here
---
+++ elpa/poporg-20170403.51/poporg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "poporg" "20170403.51" "Pop a comment or string to an empty buffer for text editing" 'nil :commit "2c58d68c81ecca4140bf179f19ed153ec804b65a" :keywords '("outlines" "tools") :url "https://github.com/QBobWatson/poporg")
